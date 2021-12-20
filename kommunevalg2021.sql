2baec506509580fd163d3bdd0e0419bc2bf409eb
godaften <73276996+godaften@users.noreply.github.com>
2021-12-21 00:59:01 +0100

    Add files via upload

---
+++ kommunevalg2021.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE 2021kommunevalg;
+SELECT * FROM 2021kommunevalg.kandidat;
+
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Marcel Meijer', 'A');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Michael Kristensen', 'A');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Helle Hansen', 'A');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Karina Knobelauch', 'A');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Anders Baun Sørensen', 'A');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Stefan Hafstein Wolffbrandt', 'A');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Robert V. Rasmussen', 'A');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Pia Ramsing', 'A');
+
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Per Urban Olsen', 'C');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Peter Askjær', 'C');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Martin Sørensen', 'C');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Louise Bramstorp', 'C');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Sigfred Jensen', 'C');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Jørn C. Nissen', 'C');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Morten Ø. Kristensen', 'C');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Susanne Andersen', 'C');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Iulian V. Paiu', 'C');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Per Hingel', 'C');
+
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Ulla Holm', 'F');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Kjeld Bønkel', 'F');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Anne Grethe Olsen', 'F');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Lone Krag', 'F');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Børge S. Buur', 'F');
+
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Per Mortensen', 'O');
+
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Søren Wiese', 'V');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Anita Elgaard Højholt Olesen', 'V');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Carsten Bruun', 'V');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Mogens Exner', 'V');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Anja Guldborg', 'V');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Klaus Holdorf', 'V');
+
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Katrine Høegh Mc Quaid', 'Ø');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Jette M. Søgaard', 'Ø');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Søren Caspersen', 'Ø');
+INSERT INTO `2021kommunevalg`.`kandidat` (`kandidat`, `parti`) VALUES ('Pia Birkmand', 'Ø');
+
+
+
+
+
+
+
+
+
